ffeea8117fa2535b7738f3657de41394d0496268
David Singleton <davidjsingleton18@gmail.com>
2025-04-09 11:00:17 -0700

    Add prototype function to fetch and display the weather information

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './style.css';
 import './reset.css';
+
+const locationName = document.querySelector('#location-name');
+const fetchWeather = document.querySelector('#fetch-weather');
+const weatherData = document.querySelector('.weather-data');
+
+fetchWeather.addEventListener('click', () => {
+    console.log(locationName.value);
+
+    weatherData.textContent = locationName.value;
+});
