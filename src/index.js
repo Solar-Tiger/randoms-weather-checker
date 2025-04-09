55178690fb0e2561acf7e01c112a13a199d87670
Solar_Tiger <davidjsingleton30@yahoo.com>
2025-04-09 15:21:24 -0700

    Add function for fetching weather data and moved reset css to the top

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import './style.css';
 import './reset.css';
+import './style.css';
 
 const locationName = document.querySelector('#location-name');
 const fetchWeather = document.querySelector('#fetch-weather');
@@ -10,3 +10,23 @@ fetchWeather.addEventListener('click', () => {
 
     weatherData.textContent = locationName.value;
 });
+
+async function fetchWeatherData() {
+    const currentDate = new Date();
+
+    const todaysDate = `${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate()}`;
+
+    const fetchedData = await fetch(
+        'https://api.allorigins.win/get?url=' +
+            encodeURIComponent(
+                `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/washington/${todaysDate}?key=LV7GDK42GAYSFKS3SEPANMFJL`
+            ),
+        { mode: 'cors' }
+    );
+
+    const weatherData = await fetchedData.json();
+
+    console.log(weatherData.contents);
+}
+
+fetchWeatherData();
