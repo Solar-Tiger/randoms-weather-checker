c678abf65c76b6532e06cf322b19698aebce1818
David Singleton <davidjsingleton18@gmail.com>
2025-04-10 11:02:47 -0700

    Update to weatherDataDisplay for sepration and clarity

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import './style.css';
 
 const locationName = document.querySelector('#location-name');
 const fetchWeather = document.querySelector('#fetch-weather');
-const weatherData = document.querySelector('.weather-data');
+const weatherDataDisplay = document.querySelector('.weather-data-display');
 
 fetchWeather.addEventListener('click', () => {
     console.log(locationName.value);
 
-    weatherData.textContent = locationName.value;
+    fetchWeatherData(locationName.value);
 });
 
 // async function fetchWeatherData() {
@@ -23,18 +23,20 @@ fetchWeather.addEventListener('click', () => {
 //             )
 //     );
 
-async function fetchWeatherData() {
+async function fetchWeatherData(location) {
     const currentDate = new Date();
 
     const todaysDate = `${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate()}`;
 
     const fetchedData = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/washington/${todaysDate}?key=LV7GDK42GAYSFKS3SEPANMFJL&include=days`
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${todaysDate}?key=LV7GDK42GAYSFKS3SEPANMFJL`
     );
 
     const weatherData = await fetchedData.json();
 
     console.log(weatherData);
-}
 
-fetchWeatherData();
+    console.log(weatherData.days[0].temp);
+
+    weatherDataDisplay.textContent = weatherData.days[0].temp;
+}
