f6b88d318094707d4fa4e9704f706ca108ce9537
David Singleton <davidjsingleton18@gmail.com>
2025-05-01 10:58:28 -0700

    Add function to display location when using weather app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@
 const locationName = document.querySelector('#location-name');
 const fetchWeather = document.querySelector('#fetch-weather');
 const weatherDataDisplay = document.querySelector('.weather-data-display');
+const weatherInformationBox = document.querySelector(
+    '.weather-information-box'
+);
 
 // Event listener for button click
 
@@ -23,10 +26,13 @@ locationName.addEventListener('keypress', (e) => {
     displayWeatherData(locationName.value);
 });
 
+// Fetches and returns weather data JSON
 async function fetchWeatherData(location) {
     const currentDate = new Date();
 
-    const todaysDate = `${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate()}`;
+    const todaysDate = `${currentDate.getFullYear()}-${
+        currentDate.getMonth() + 1
+    }-${currentDate.getDate()}`;
 
     const fetchedData = await fetch(
         `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${todaysDate}?key=LV7GDK42GAYSFKS3SEPANMFJL`
@@ -37,11 +43,13 @@ async function fetchWeatherData(location) {
     return weatherData;
 }
 
+// Displays fetched weather data using location name
 async function displayWeatherData(location) {
     const fetchedWeatherData = await fetchWeatherData(location);
 
-    // console.log(fetchedWeatherData.days[0]);
-    console.log(fetchedWeatherData.days[0].icon);
+    weatherInformationBox.textContent = '';
+
+    displayWeatherLocation(fetchedWeatherData);
 
     const weatherIcon = displayCurrentWeatherIcon(
         fetchedWeatherData.days[0].icon
@@ -60,11 +68,24 @@ function displayCurrentWeatherIcon(currentWeather) {
     return weatherIconImg;
 }
 
+async function displayWeatherLocation(location) {
+    const nameDisplay = document.createElement('p');
+
+    nameDisplay.classList.add('weather-location');
+    nameDisplay.textContent = location.resolvedAddress;
+
+    weatherInformationBox.appendChild(nameDisplay);
+}
+
+// async function displayWeatherData() {
+//     weatherInformationBox.textContent = '';
+// }
+
 /*  
     Relevant information for user
 
     1.) Location Name. Done
-    2.) Conditions(snow, rain, fog, wind, cloudy, partly-cloudy-day/partly-cloudy-night, clear-day/clear-night)
+    2.) Conditions(snow, rain, fog, wind, cloudy, partly-cloudy-day/partly-cloudy-night, clear-day/clear-night). Done
     3.) Description. Done
     4.) Feelslike, feelslikemax, feelslike min
     5.) Temp, tempmax, tempmin. Done
