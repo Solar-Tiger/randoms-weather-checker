ada9e0362f904ee9ec98ef4c5023bedaf46735b3
David Singleton <davidjsingleton18@gmail.com>
2025-05-10 13:13:53 -0700

    Update toggle temperature function to change degrees and button name

--- src/toggleTemp.js
+++ src/toggleTemp.js
@@ -1,7 +1,40 @@
-function toggleTemp() {
-    const currentTemperature = document.querySelector('.current-temperature');
+import { convertTemperature } from './tempConverter';
 
-    console.log(currentTemperature);
+function toggleTemp() {
+    const currentTempScale = document.querySelector(
+        '#change-temp > span'
+    ).textContent;
+
+    let currentTemp = document.querySelector('.current-temperature');
+    let maxTemp = document.querySelector('.max-temp');
+    let minTemp = document.querySelector('.min-temp');
+
+    let updatedCurrentTemp = convertTemperature(
+        Number(currentTemp.textContent),
+        currentTempScale
+    );
+    let updatedMaxTemp = convertTemperature(
+        Number(maxTemp.textContent),
+        currentTempScale
+    );
+    let updatedMinTemp = convertTemperature(
+        Number(minTemp.textContent),
+        currentTempScale
+    );
+
+    currentTemp.textContent = updatedCurrentTemp;
+    maxTemp.textContent = updatedMaxTemp;
+    minTemp.textContent = updatedMinTemp;
 }
 
-export { toggleTemp };
+function changeTempScaleDisplay() {
+    let currentTempScaleDisplay = document.querySelector('#change-temp > span');
+
+    if (currentTempScaleDisplay.textContent === 'Celsius') {
+        currentTempScaleDisplay.textContent = 'Fahrenheit';
+    } else {
+        currentTempScaleDisplay.textContent = 'Celsius';
+    }
+}
+
+export { toggleTemp, changeTempScaleDisplay };
