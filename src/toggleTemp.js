5fd77bd74cde979f5bc37796f7a97c268064eda6
David Singleton <davidjsingleton18@gmail.com>
2025-06-02 10:41:39 -0700

    Remove uneeded function

--- src/toggleTemp.js
+++ src/toggleTemp.js
@@ -23,32 +23,6 @@ function toggleTemp(currentTempScale) {
     minTemp.textContent = updatedMinTemp;
 }
 
-function toggleAllTemps(currentTempScale) {
-    let currentTemp = document.querySelector('.current-temperature');
-    let maxTemp = document.querySelectorAll('.temp-max');
-    let minTemp = document.querySelectorAll('.temp-min');
-    let weatherConditions = document.querySelectorAll(
-        '.weather-conditions-current'
-    );
-
-    let updatedCurrentTemp = convertTemperature(
-        Number(currentTemp.textContent),
-        currentTempScale
-    );
-    let updatedMaxTemp = convertTemperature(
-        Number(maxTemp.textContent),
-        currentTempScale
-    );
-    let updatedMinTemp = convertTemperature(
-        Number(minTemp.textContent),
-        currentTempScale
-    );
-
-    currentTemp.textContent = updatedCurrentTemp;
-    maxTemp.textContent = updatedMaxTemp;
-    minTemp.textContent = updatedMinTemp;
-}
-
 function changeTempScaleDisplay(currentTempScaleDisplay) {
     if (currentTempScaleDisplay === 'Celsius') {
         return 'Fahrenheit';
