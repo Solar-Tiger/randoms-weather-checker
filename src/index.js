e30c9552307a93fb5e82ddad59dc17929a57eac0
David Singleton <davidjsingleton18@gmail.com>
2025-06-02 10:53:30 -0700

    Add more descriptive comments for event listeners

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { displayWeatherData, changeTempScale } from './weatherApp';
 const locationName = document.querySelector('#location-name');
 const getWeather = document.querySelector('#get-weather');
 
-// Event listener for button click
+// Event listener for button click to fetch weather data
 getWeather.addEventListener('click', () => {
     displayWeatherData(locationName.value);
 });
 
-// Event listener for enter key
+// Event listener for enter key to fetch weather data
 locationName.addEventListener('keypress', (e) => {
     if (e.key !== 'Enter') return;
 
